4f58a05c471ef04cffb32148c95c55648fba38c9
mainzonder <no7au53r@gmail.com>
2022-01-25 19:23:15 +0200

    unit-016

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,25 +1,58 @@
+import { Component } from 'react';
+
 import "./employer-add-form.css"
 
-const EmployersAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h2>Добавьте нового сотрудника</h2>
-            
-             <form className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+class EmployersAddForm extends Component {
 
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $" />
-
-                <button className="btn btn-outline-light"> Добавить</button>
-            </form>
-        </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: 0
+        }
+    }
 
 
-    )
+    onValueChange=(e)=>{
+this.setState({
+    [e.target.name ]: e.target.value
+})
+
+    }
+
+
+    render() {
+
+const{name, salary} = this.state
+
+
+        return (
+            <div className="app-add-form">
+                <h2>Добавьте нового сотрудника</h2>
+
+                <form className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name="name"
+                        value={name}
+                        onChange = {this.onValueChange}
+                    />
+
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $"
+                        name="salary"
+                        value={salary}
+                        onChange={this.onValueChange}
+                    />
+
+                    <button className="btn btn-outline-light"> Добавить</button>
+                </form>
+            </div>
+
+
+        )
+    }
 }
-
 export default EmployersAddForm
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,23 +1,27 @@
 import EmployersListItem from '../employers-list-item/employers-list-item'
 import './employers-list.css'
 
-const EmployersList = ({ data }) => {
+const EmployersList = ({data}) => {
 
 
-        const elements = data.map(item => {
-                return (
-                        <EmployersListItem {...item}/>
-                )
-        })
+    const elements = data.map((item ) => {
+      const {id, ...itemProps} = item
 
         return (
-                <ul className="app-list list-group">
-                        {elements}
-                </ul>
-
-
-
+            <EmployersListItem key={id} {...itemProps}/>
         )
+    })
+
+
+
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+
+
+    )
 }
 
 export default EmployersList;
